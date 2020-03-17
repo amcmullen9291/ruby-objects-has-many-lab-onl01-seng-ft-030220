57a19071d602d4581eda2f9b59f564c9acc697a1
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-17 00:11:01 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,8 +17,8 @@ attr_accessor :name, :post
     post.author = self 
   end
   
-  def add_post_by_title(post_title) 
-    name= Post.new(post_title)
+  def add_post_by_title(name) 
+    name= Post.new(name)
     add_post(name)
   end 
 
